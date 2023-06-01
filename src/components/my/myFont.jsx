a8af8b6793eb67283d91718b09ce79d5b03097ec
Fedir Melnyk (trusthemind) <pluserwork24@gmail.com>
2023-06-01 11:05:46 +0300

    selected fix commit

--- src/components/my/myFont.jsx
+++ src/components/my/myFont.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { tabsApi } from '../service/apiSlice';
-import "./style.css"
+import './style.css';
 
 function MyFont() {
   const { data, isLoading } = tabsApi.useGetFont_AQuery();
-  useEffect(() => {
-  }, [data])
+  const [activeIndex, setActiveIndex] = useState();
+  useEffect(() => { }, [data]);
 
   if (isLoading) {
     return <div id="loading"></div>;
@@ -16,12 +16,18 @@ function MyFont() {
       <h1>{data.type}</h1>
       <div id='fonts'>
         {data.content.map((item, index) => (
-          <div id='cardItem' key={index}>
-            <div id="item" style={{ backgroundColor: item.color }}><h1 id='abbr'>{item.abbr}</h1></div>
-            <ul ><li>{item.label}</li></ul>
-          </div>))}
+          <div id='cardItem' onClick={() => setActiveIndex(index)} className={activeIndex === index ? 'active' : "unactive"} key={index}>
+            <div id='item' style={{ backgroundColor: item.color }}>
+              <h1 id='abbr'>{item.abbr}</h1>
+            </div>
+            <ul>
+              <li>{item.label}</li>
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
+
 export default MyFont;
